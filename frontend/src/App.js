5b2ad487a05e5a6a0b8fc35ecb6bf5d17f47a236
Svetlana Tomašik <seka.corazon@gmail.com>
2023-03-04 03:32:39 +0100

    output a list to the events page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: "events", element: <EventsPage /> },
-      { path: "events/eventId", element: <EventDetailPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
       { path: "events/new", element: <NewEventPage /> },
       { path: "events/:eventId/edit", element: <EditEventPage /> },
     ],
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e2",
+    title: "Some event",
+  },
+];
+
 const EventsPage = () => {
-    return <h1>EventsPage</h1>
-}
+  return (
+    <>
+      <h1>EventsPage</h1>;
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+            {/* convert it to a relative link by just navigating to event.id */}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default EventsPage;
