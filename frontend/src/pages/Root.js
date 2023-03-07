fdc8b318f8dd19b9ec97fbae8dab2ca7bc7f1aa4
Svetlana Tomašik <seka.corazon@gmail.com>
2023-03-07 07:13:52 +0100

    returning responses in loader()

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -3,12 +3,12 @@ import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 const RootLayout = () => {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
 
   return (
     <>
       <MainNavigation />
-      {navigation.state === "loading" && <p>Loading...</p>}
+      {/* {navigation.state === "loading" && <p>Loading...</p>} */}
       <main>
         <Outlet />
       </main>
