66ca94456c3013fdf06a54ee78d2562f64400909
Svetlana Tomašik <seka.corazon@gmail.com>
2023-03-07 08:11:03 +0100

    extracting error data and throwing responses

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,31 @@
+import { useRouteError } from "react-router-dom";
+
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 const ErrorPage = () => {
-  return <h1>An error occured!</h1>;
+  const error = useRouteError();
+
+  let title = "An error occurred";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found";
+    message = "Could not find a resourse or page.";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 };
 
 export default ErrorPage;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -16,7 +16,9 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw { message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     return response;
   }
