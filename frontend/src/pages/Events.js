c5185165b9bc220ec6a147d201afd71e6dab3639
Svetlana Tomašik <seka.corazon@gmail.com>
2023-03-07 07:39:13 +0100

    handling error (alternative)

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,10 +5,6 @@ import EventsList from "../components/EventsList";
 function EventsPage() {
   const data = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
-
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -20,7 +16,7 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    return { isError: true, message: "Could not fetch events." };
+    throw { message: "Could not fetch events." };
   } else {
     return response;
   }
